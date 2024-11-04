6dcd82a9979f35da8bd0796f0d5d4fd988d3c032
sashaYanushevich <rewqsasha217@gmail.com>
2024-11-04 21:48:06 +0300

    fix bugs

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,9 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = "YOUR_SECRET_KEY"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    DATABASE_URL: str = "mysql+aiomysql://Managment:ivory1236@localhost:3306/Managment"
+    DATABASE_URL: str = "postgresql+asyncpg://postgres:657660@localhost:5433/ManagmentApp"
+
+    # DATABASE_URL: str = "mysql+aiomysql://Managment:ivory1236@localhost:3306/Managment"
     EMAIL_RESET_TOKEN_EXPIRE_HOURS:int = 1
     URL:str = "http://localhost:8000"
 
